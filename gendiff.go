0dce0a0aa186eb588fd6646c495c527908afbd16
Рустэм Хушмурадов <rikhushmur@mts.ru>
2026-02-20 14:44:25 +0500

    доработал по тестам 10

--- gendiff.go
+++ gendiff.go
@@ -1,7 +1,6 @@
 package code
 
 import (
-	"encoding/json"
 	"fmt"
 
 	parser "code/parser"
@@ -10,31 +9,27 @@ import (
 
 
 func GenDiff(path1, path2, format string) (string, error) {
-	data1, err := parser.ParseFile(path1)
-	if err != nil {
-		return "", err
-	}
+    data1, err := parser.ParseFile(path1)
+    if err != nil {
+        return "", err
+    }
 
-	data2, err := parser.ParseFile(path2)
-	if err != nil {
-		return "", err
-	}
+    data2, err := parser.ParseFile(path2)
+    if err != nil {
+        return "", err
+    }
 
-	diff := formatter.BuildDiff(data1, data2)
+    diff := formatter.BuildDiff(data1, data2)
 
-	switch format {
-	case "stylish":
-		return formatter.FormatStylish(diff, 0), nil
-	case "plain":
-		return formatter.FormatPlain(diff, ""), nil
-	case "json":
-		raw, err := json.MarshalIndent(diff, "", "  ")
-		if err != nil {
-			return "", fmt.Errorf("failed to format JSON: %w", err)
-		}
-		return string(raw), nil
-	default:
-		return "", fmt.Errorf("unsupported format: %s", format)
-	}
+    switch format {
+    case "stylish":
+        return formatter.FormatStylish(diff, 0), nil
+    case "plain":
+        return formatter.FormatPlain(diff, ""), nil
+    case "json":
+        return formatter.FormatJSON(diff)
+    default:
+        return "", fmt.Errorf("unsupported format: %s", format)
+    }
 }
 
